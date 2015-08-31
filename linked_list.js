dcbd7dc1927e3870fe9748f985320b7031bb9581
brettgrigsby <gbrett@gmail.com>
2015-08-31 13:03:51 -0600

    did it all

--- linked_list.js
+++ linked_list.js
@@ -1,7 +1,191 @@
 function createList() {
-  return { head: null };
+  return {
+    head: null,
+    makeNode: function(data) {
+      return {
+        data: data,
+        push:function(node) {
+          if(this.link) {
+            this.link.push(node);
+          } else {
+            this.link = node;
+          };
+        },
+        insert: function(node, index, count) {
+          if(index === count) {
+            node.link = this.link;
+            this.link = node;
+          } else {
+            this.link.insert(node, index, count + 1);
+          };
+        },
+        include: function(data) {
+          if(this.data === data) {
+            return true;
+          } else if(this.link) {
+            return link.include(data);
+          } else {
+            return false;
+          };
+        },
+        pop: function() {
+          if(this.link.link) {
+            return this.link.pop();
+          } else {
+            var value = this.link.data;
+            this.link = null;
+            return value;
+          };
+        },
+        last: function() {
+          if(this.link.link) {
+            return this.link.last();
+          } else {
+            return this.link.data;
+          };
+        },
+        find_by_index: function(index, count) {
+          if(index === count) {
+            return this.data;
+          } else if(this.link) {
+            return this.link.find_by_index(index, count + 1);
+          } else {
+            return null;
+          };
+        },
+        find_by_value: function(value, index) {
+          if(this.data === value) {
+            return index;
+          } else if(this.link) {
+            return this.link.find_by_value(value, index + 1);
+          } else {
+            return null;
+          };
+        },
+        remove_by_index: function(index, count) {
+          if(index === count && this.link) {
+            var value = this.link.data;
+            this.link = this.link.link;
+            return value;
+          } else if (this.link) {
+            return this.link.remove_by_index(index, count + 1);
+          } else {
+            return null;
+          };
+        },
+        remove_by_value: function(value) {
+          if(this.link && this.link.data === value) {
+            this.link = this.link.link;
+          } else if(this.link) {
+            thic.link.remove_by_value(value);
+          };
+        },
+        count: function() {
+          if(this.link) {
+            return this.link.count() + 1;
+          } else {
+            return 1;
+          };
+        }
+      };
+    }
+  };
 }
 
 function push(list, data) {
-  // your code here :)
+  var newNode = list.makeNode(data);
+  if(list.head === null) {
+    list.head = newNode;
+  } else {
+    list.head.push(newNode);
+  };
+}
+
+function shift(list, data) {
+  var newNode = list.makeNode(data);
+  newNode.link = list.head;
+  list.head = newNode;
+}
+
+function insert(list, data, index) {
+  var newNode = list.makeNode(data);
+  if(index === 0) {
+    shift(list, newNode);
+  } else {
+    list.head.insert(newNode, index, 1);
+  };
+}
+
+function include(list, data) {
+  if(list.head) {
+    return list.head.include(data);
+  } else {
+    return false;
+  }
+}
+
+function pop(list) {
+  if(list.head.link) {
+    return list.head.pop();
+  } else if(list.head) {
+    var value = list.head.data;
+    list.head = null;
+    return value;
+  } else {
+    return null;
+  };
+}
+
+function last(list) {
+  if(list.head.link) {
+    return list.head.last();
+  } else if(list.head) {
+    return list.head.data;
+  } else {
+    return null;
+  };
+}
+
+function find_by_index(list, index) {
+  if(list.head) {
+    return list.head.find_by_index(index, 0);
+  } else {
+    return null;
+  };
+}
+
+function find_by_value(list, value) {
+  if(list.head) {
+    return list.head.find_by_value(value, 0);
+  } else {
+    return null;
+  };
+}
+
+function remove_by_index(list, index) {
+  if(index === 0 && list.head) {
+    var value = list.head.data;
+    list.head = list.head.link;
+    return value;
+  } else if (list.head) {
+    return list.head.remove_by_index(index, 1);
+  } else {
+    return null;
+  };
+}
+
+function remove_by_value(list, value) {
+  if(list.head && list.head.data === value) {
+    list.head = list.head.link;
+  } else if(list.head) {
+    list.head.remove_by_value(value);
+  };
+}
+
+function count(list) {
+  if(list.head) {
+    return list.head.count();
+  } else {
+    return 0;
+  };
 }
--- linked_list_test.js
+++ linked_list_test.js
@@ -11,4 +11,106 @@ describe('testing', function () {
     push(list, "pizza");
     assert.equal(list.head.data, "pizza");
   });
+
+  it('appends an item to the end of the list', function() {
+    var list = createList();
+    push(list, "pizza");
+    push(list, "fries");
+    assert.equal(list.head.link.data, "fries");
+  });
+
+  it('prepends an item to the beginning of the list', function() {
+    var list = createList();
+    push(list, "pizza");
+    shift(list, "fries");
+    assert.equal(list.head.data, "fries");
+  });
+
+  it('prepends an item to the beginning of an empty list', function() {
+    var list = createList();
+    shift(list, "fries");
+    assert.equal(list.head.data, "fries");
+  });
+
+  it('inserts an item at a given index', function() {
+    var list = createList();
+    push(list, "fries");
+    push(list, "pizza");
+    push(list, "jam");
+    insert(list, "twix", 2)
+    assert.equal(list.head.link.link.data, "twix");
+    assert.equal(list.head.link.link.link.data, "jam");
+  });
+
+  it('returns if data is included in list', function() {
+    var list = createList();
+    push(list, "fries");
+    assert(include(list, "fries"));
+    assert.equal(include(list, "tacos"), false);
+  });
+
+  it('returns total count for list', function() {
+    var list = createList();
+    assert.equal(count(list), 0);
+
+    push(list, "fries");
+    push(list, "ries");
+    push(list, "ies");
+    push(list, "es");
+    assert.equal(count(list), 4);
+  });
+
+  it('can pop off the last element of the list', function() {
+    var list = createList();
+    push(list, "pizza");
+    push(list, "coke");
+    push(list, "fries");
+    assert.equal(pop(list), "fries");
+    assert.equal(list.head.link.link, null);
+  });
+
+  it('returns last value in list', function() {
+    var list = createList();
+    push(list, "fries");
+    push(list, "milkshakes");
+    assert.equal(last(list), "milkshakes");
+  });
+
+  it('can find a value by its index', function() {
+    var list = createList();
+    push(list, "pizza");
+    push(list, "coke");
+    push(list, "fries");
+    assert.equal(find_by_index(list, 1), "coke");
+    assert.equal(find_by_index(list, 2), "fries");
+    assert.equal(find_by_index(list, 5), null);
+  });
+
+  it('can find an index by its value', function() {
+    var list = createList();
+    push(list, "pizza");
+    push(list, "coke");
+    push(list, "fries");
+    assert.equal(find_by_value(list, "coke"), 1);
+    assert.equal(find_by_value(list, "fries"), 2);
+    assert.equal(find_by_value(list, "cookies"), null);
+  });
+
+  it('can remove an element by index', function() {
+    var list = createList();
+    push(list, "pizza");
+    push(list, "coke");
+    push(list, "fries");
+    assert.equal(remove_by_index(list, 1), "coke");
+    assert.equal(list.head.link.data, "fries");
+  });
+
+  it('can remove an element by value', function() {
+    var list = createList();
+    push(list, "pizza");
+    push(list, "coke");
+    push(list, "fries");
+    remove_by_value(list, "coke");
+    assert.equal(list.head.link.data, "fries");
+  });
 });
